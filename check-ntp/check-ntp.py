aac826991d2a356d1bc6dddfabe821d071f147b0
David Taylor <dave@imagetaylor.com>
2020-12-22 18:56:18 +1100

    updated with outputs

--- check-ntp/check-ntp.py
+++ check-ntp/check-ntp.py
@@ -7,7 +7,7 @@ import argparse
 
 
 # Set up command line parser
-parser = argparse.ArgumentParser(description='Format: check-ntp.py -r -R host')
+parser = argparse.ArgumentParser(description='Format: check-ntp.py -r -R')
 parser.add_argument('-r', '--rttok', type=float, default='25',
                     help='Upper rtt  limit considered ok')
 parser.add_argument('-R', '--rttmax', type=float, default='50',
@@ -18,11 +18,22 @@ args = parser.parse_args()
 rttok = vars(args)['rttok']
 rttmax = vars(args)['rttmax']
 
-## Pseudo code
-# get list of configured servers and status
+# check for active server and get metrics
 source = re.split(r'\s+',os.system('chronyc -n sources | grep *'))
 
-# for for active server get metrics
-# create dictionary based on metric list, split and remove blanc spaces
-# check values for latency, jitter against arg values
-# output status and latency
+if source 
+    if source[5] < rttok
+        print(f"HOST OK - rtt = {source[5]} ms", end = '')
+        sys.exit(0)
+    elif source[5] < rttok && source[5] > rttmax
+        print(f"HOST WARN - rtt = {source[5]} ms", end = '')
+        sys.exit(1)
+    elif source[5] < rttok && source[5] > rttmax
+        print(f"HOST CRITICAL - rtt = {source[5]} ms", end = '')
+        sys.exit(2)
+    else source[5] < ttmax
+        print(f"HOST CRITICAL - Down", end = '')
+        sys.exit(0)
+else source[5] < ttmax
+    print(f"HOST CRITICAL - Down", end = '')
+    sys.exit(0)
