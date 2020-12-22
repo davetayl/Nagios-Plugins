0f2c2c3f8c10f99f998961955408a22771b2e5d6
root <root@localhost.localdomain>
2020-12-22 09:22:11 +0000

    all basics working

--- check-ntp/check-ntp.py
+++ check-ntp/check-ntp.py
@@ -1,13 +1,14 @@
 #!/bin/env python3
 
 # Import libraries
+import sys
 import os
 import re
 import argparse
 
 
 # Set up command line parser
-parser = argparse.ArgumentParser(description='Format: check-ntp.py -r -R')
+parser = argparse.ArgumentParser(description='Format: check-ntp.py -r -R -h')
 parser.add_argument('-r', '--rttok', type=float, default='25',
                     help='Upper rtt  limit considered ok')
 parser.add_argument('-R', '--rttmax', type=float, default='50',
@@ -19,21 +20,22 @@ rttok = vars(args)['rttok']
 rttmax = vars(args)['rttmax']
 
 # check for active server and get metrics
-source = re.split(r'\s+',os.system('chronyc -n sources | grep *'))
+source = re.split(r'\s+',str(os.popen('chronyc -n sources | grep \*').read()))
+source = float(source[9].replace('ms',''))
 
-if source 
-    if source[5] < rttok
-        print(f"HOST OK - rtt = {source[5]} ms", end = '')
+if source != 0:
+    if source < rttok:
+        print(f"HOST OK - rtt = {source}ms")
         sys.exit(0)
-    elif source[5] < rttok && source[5] > rttmax
-        print(f"HOST WARN - rtt = {source[5]} ms", end = '')
+    elif source > rttok and source < rttmax:
+        print(f"HOST WARN - rtt = {source}ms")
         sys.exit(1)
-    elif source[5] < rttok && source[5] > rttmax
-        print(f"HOST CRITICAL - rtt = {source[5]} ms", end = '')
+    elif source > rttmax:
+        print(f"HOST CRITICAL - rtt = {source}ms")
         sys.exit(2)
-    else source[5] < ttmax
-        print(f"HOST CRITICAL - Down", end = '')
+    else:
+        print(f"HOST CRITICAL - Down")
         sys.exit(0)
-else source[5] < ttmax
-    print(f"HOST CRITICAL - Down", end = '')
+else:
+    print(f"HOST CRITICAL - Unknown")
     sys.exit(0)
