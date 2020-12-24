fd10594da1a362d84984de270f537051a00b3250
David Taylor <dave@imagetaylor.com>
2020-12-25 05:34:41 +1100

    added validation

--- check-ntp/check-ntp.py
+++ check-ntp/check-ntp.py
@@ -21,21 +21,25 @@ rttmax = vars(args)['rttmax']
 
 # check for active server and get metrics
 source = re.split(r'\s+',str(os.popen('chronyc -n sources | grep \*').read()))
-source = float(source[9].replace('ms',''))
-
-if source != 0:
-    if source < rttok:
-        print(f"HOST OK - rtt = {source}ms")
-        sys.exit(0)
-    elif source > rttok and source < rttmax:
-        print(f"HOST WARN - rtt = {source}ms")
-        sys.exit(1)
-    elif source > rttmax:
-        print(f"HOST CRITICAL - rtt = {source}ms")
-        sys.exit(2)
+if source[0] contains *:
+    source = float(source[9].replace('ms',''))
+    
+    if source != 0:
+        if source < rttok:
+            print(f"HOST OK - rtt = {source}ms")
+            sys.exit(0)
+        elif source > rttok and source < rttmax:
+            print(f"HOST WARN - rtt = {source}ms")
+            sys.exit(1)
+        elif source > rttmax:
+            print(f"HOST CRITICAL - rtt = {source}ms")
+            sys.exit(2)
+        else:
+            print(f"HOST CRITICAL - Down")
+            sys.exit(0)
     else:
         print(f"HOST CRITICAL - Down")
         sys.exit(0)
 else:
-    print(f"HOST CRITICAL - Unknown")
-    sys.exit(0)
+    print(f"HOST CRITICAL - Down")
+    sys.exit(0)    
